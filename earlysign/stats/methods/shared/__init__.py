f7d2f958cc8e4c8b4e7b967d3348d879799117db
Takeshi Teshima <takeshi.78.teshima@gmail.com>
2025-09-09 02:51:41 +0900

    Cleanup code

--- earlysign/stats/methods/shared/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Shared utilities and components across statistical methods.
-
-This module contains common utilities, data models, and helper functions
-that are used across different statistical testing methods.
-"""
