20cc0b047e27bd9dc4c585e046bc21cfe4962d8e
Takeshi Teshima <takeshi.78.teshima@gmail.com>
2025-10-21 02:11:52 +0900

    Refactor

--- earlysign/core/ledger.py
+++ earlysign/core/ledger.py
@@ -217,4 +217,10 @@ class Ledger:
         if all:
             return self.t.execute()
         else:
-            return self.t.order_by("ts").mutate(payload_type=self.t.payload_type.cast("string").split(".")[-1]).rename({"payload_name": "payload_type"}).drop("uuid", "ts", "pkg_version").execute()
+            return (
+                self.t.order_by("ts")
+                .mutate(payload_type=self.t.payload_type.cast("string").split(".")[-1])
+                .rename({"payload_name": "payload_type"})
+                .drop("uuid", "ts", "pkg_version")
+                .execute()
+            )
