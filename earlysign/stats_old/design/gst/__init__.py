418e7a22716308b901e9146bb333dbe5693907dc
Takeshi Teshima <takeshi.78.teshima@gmail.com>
2025-11-23 15:04:41 +0900

    Remove design widget code

--- earlysign/stats_old/design/gst/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Group Sequential Trial (GST) design module.
-
-Provides both interactive UI and programmatic API for designing
-group sequential trials with various spending functions and optimization modes.
-"""
