3ff7be9f5b745e9236105954c156cf514046e374
Takeshi Teshima <takeshi.78.teshima@gmail.com>
2026-01-31 17:18:40 +0900

    Update doc author visibility

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,7 +7,7 @@ sys.path.insert(0, os.path.abspath("../.."))
 sys.path.insert(0, os.path.abspath("."))
 
 project = "EarlySign"
-author = "Takeshi Teshima"
+author = "EarlySign Developers"
 
 extensions = [
     "sphinx.ext.autodoc",
